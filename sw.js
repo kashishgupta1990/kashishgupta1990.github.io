53bc72b4d01a76af6a2b6b1725757191db3e6de0
Kashish Gupta <kashishgupta1990@yahoo.com>
2015-12-26 16:24:48 +0530

    bugFix

--- sw.js
+++ sw.js
@@ -27,6 +27,6 @@ this.addEventListener('fetch', function(event) {
     });
     return response.clone();
   }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    return caches.match('/gallery/myLittleVader.jpg');
   }));
 });
