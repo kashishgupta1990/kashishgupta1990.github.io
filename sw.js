512cf98841d9da6f2589637e4d049633ebfb0905
Kashish Gupta <kashishgupta1990@yahoo.com>
2015-12-28 11:34:53 +0530

    added consoles

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 this.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('v1').then(function (cache) {
+
+            console.log('Creating cache of the static files');
+
             return cache.addAll([
                 '/',
                 '/index.html',
